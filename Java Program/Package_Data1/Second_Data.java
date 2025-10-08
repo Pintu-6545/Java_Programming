b652185e4ad8ed573e18b3a9e2302d537f9e8922
Pintu-6545 <pmodi7700@gmail.com>
2025-10-08 22:37:31 +0530

    .

---
+++ Java Program/Package_Data1/Second_Data.java
@@ -0,0 +1,8 @@
+package Package_Data1;
+
+public class Second_Data {
+  public void  getView1()
+  {
+	  System.out.println("Welcome to Python Programming Language");
+  }
+}
---
+++ Java Program/Package_Management.java
@@ -0,0 +1,10 @@
+import Package_Data.First_Data;
+import Package_Data1.Second_Data;
+public class Package_Management {
+    public static void main(String[] args) {
+		First_Data  f1 = new First_Data();
+		Second_Data s1 = new Second_Data();
+		f1.getView();
+		s1.getView1();
+	}
+}
