440876176761e781f5f65bbf834d71efff078154
Pintu-6545 <pmodi7700@gmail.com>
2025-11-13 19:39:42 +0530

    commit

---
+++ Online Employeement Management System/Employee_Data/src/main/java/com/bean/Employee.java
@@ -0,0 +1,62 @@
+package com.bean;
+
+public class Employee {
+     private int emp_id;
+     private String fname,lname,gender,email,address,city,state;
+     private long mobile;
+	 public int getEmp_id() {
+		 return emp_id;
+	 }
+	 public void setEmp_id(int emp_id) {
+		 this.emp_id = emp_id;
+	 }
+	 public String getFname() {
+		 return fname;
+	 }
+	 public void setFname(String fname) {
+		 this.fname = fname;
+	 }
+	 public String getLname() {
+		 return lname;
+	 }
+	 public void setLname(String lname) {
+		 this.lname = lname;
+	 }
+	 public String getGender() {
+		 return gender;
+	 }
+	 public void setGender(String gender) {
+		 this.gender = gender;
+	 }
+	 public String getEmail() {
+		 return email;
+	 }
+	 public void setEmail(String email) {
+		 this.email = email;
+	 }
+	 public String getAddress() {
+		 return address;
+	 }
+	 public void setAddress(String address) {
+		 this.address = address;
+	 }
+	 public String getCity() {
+		 return city;
+	 }
+	 public void setCity(String city) {
+		 this.city = city;
+	 }
+	 public String getState() {
+		 return state;
+	 }
+	 public void setState(String state) {
+		 this.state = state;
+	 }
+	 public long getMobile() {
+		 return mobile;
+	 }
+	 public void setMobile(long mobile) {
+		 this.mobile = mobile;
+	 }
+     
+}
